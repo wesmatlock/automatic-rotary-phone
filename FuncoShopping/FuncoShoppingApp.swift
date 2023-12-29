fa0140a92161653be15acbf0d3b77af20bc7c445
Wes Matlock <wmatlock@gmail.com>
2023-12-29 13:16:39 -0700

    Initial Commit

---
+++ FuncoShopping/FuncoShoppingApp.swift
@@ -0,0 +1,17 @@
+//
+//  FuncoShoppingApp.swift
+//  FuncoShopping
+//
+//  Created by Wesley Matlock on 12/29/23.
+//
+
+import SwiftUI
+
+@main
+struct FuncoShoppingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
